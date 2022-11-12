1c0faa8d473eb4830e3cdcb7385a2d445c597b42
gustavohpaez <gustavopaezh@gmail.com>
2022-11-12 00:32:06 -0300

    Modificamos carro para que solo se vea si estas autenticado

--- carro/context_processors.py
+++ carro/context_processors.py
@@ -8,6 +8,7 @@ def importe_total_carro(request):
     if request.user.is_authenticated:
         for key, value in request.session["carro"].items():
             total=total+float(value["precio"])
+
     return {"importe_total_carro":total}
 
 
